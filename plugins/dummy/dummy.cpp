761f29274fa934d62d0a9a46d525059b79374863
Carles Bolaños <carlesalbertbg@gmail.com>
2022-08-09 16:56:33 +0200

    Add files via upload

--- plugins/dummy/dummy.cpp
+++ plugins/dummy/dummy.cpp
@@ -1,4 +1,81 @@
 #include "dummy.h"
+#include <thread>
+
+class dummyWorker
+{
+public:
+  virtual bool load(const char *param) = 0;
+  bool abort(const char *jobUID)
+  {
+    m_abort = true;
+    return true;
+  }
+  virtual bool process(const char *job, const std::function<void(int)> &onProgress, const std::function<void(const char *, const char *)> &onCompleted) = 0;
+
+protected:
+  bool m_abort = false;
+};
+
+/*
+ *
+ */
+class dummy1Worker : public dummyWorker
+{
+public:
+  dummy1Worker()
+  {
+
+  }
+
+  bool load(const char *param)
+  {
+    return true;
+  }
+
+  bool process(const char *job, const std::function<void(int)> &onProgress, const std::function<void(const char *, const char *)> &onCompleted)
+  {
+    // process
+    for(int i = 0; i < 100 && !m_abort; i += 10)
+    {
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+
+      // status
+      onProgress(i);
+    }
+
+    // status
+    onCompleted("" /* error */, "" /* condition */);
+
+    return true;
+  }
+};
+
+/*
+ *
+ */
+class dummy2Worker : public dummyWorker
+{
+public:
+  dummy2Worker()
+  {
+
+  }
+
+  bool load(const char *param)
+  {
+    return true;
+  }
+
+  bool abort(const char *jobUID)
+  {
+    return true;
+  }
+
+  bool process(const char *job, const std::function<void(int)> &onProgress, const std::function<void(const char *, const char *)> &onCompleted)
+  {
+    return true;
+  }
+};
 
 /*
  *
@@ -33,27 +110,25 @@ const SPluginWorker * register_workers()
 
 void * create_worker(const char *type)
 {
+  if(!strcmp(type, "dummy_1"))
+    return new dummy1Worker;
+  if(!strcmp(type, "dummy_2"))
+    return new dummy2Worker;
+
   return 0;
 }
 
 bool destroy_worker(void *w)
 {
-  return true;
-}
+  delete w;
 
-bool start_worker(void *w)
-{
-  return true;
-}
-
-bool stop_worker(void *w)
-{
   return true;
 }
 
 bool load_worker(void *w, const char *param)
 {
-  return true;
+  dummyWorker *dw = static_cast<dummyWorker *> (w);
+  return dw->load(param);
 }
 
 void log_set_callback(void(*fn) (ELogSeverity severity, const char *message, void *_private), void *param)
@@ -61,3 +136,14 @@ void log_set_callback(void(*fn) (ELogSeverity severity, const char *message, voi
   wf_log_set_callback(fn, param);
 }
 
+bool abort_job(void *w, const char *jobUID)
+{
+  dummyWorker *dw = static_cast<dummyWorker *> (w);
+  return dw->abort(jobUID);
+}
+
+bool process_job(void *w, const char *job, const std::function<void(int)> &onProgress, const std::function<void(const char *, const char *)> &onCompleted)
+{
+  dummyWorker *dw = static_cast<dummyWorker *> (w);
+  return dw->process(job, onProgress, onCompleted);
+}
--- plugins/dummy/dummy.h
+++ plugins/dummy/dummy.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include "plugin_helper.h"
 #include "logger.h"
 
@@ -15,6 +16,6 @@ extern "C" DUMMY_API const SPluginWorker * register_workers();
 extern "C" DUMMY_API void log_set_callback(void (*fn) (ELogSeverity severity, const char *message, void *_private), void *param);
 extern "C" DUMMY_API void * create_worker(const char *type);
 extern "C" DUMMY_API bool destroy_worker(void *w);
-extern "C" DUMMY_API bool start_worker(void *w);
-extern "C" DUMMY_API bool stop_worker(void *w);
 extern "C" DUMMY_API bool load_worker(void *w, const char *param);
+extern "C" DUMMY_API bool abort_job(void *w, const char *jobUID);
+extern "C" DUMMY_API bool process_job(void *w, const char *job, const std::function<void(int)> &onProgress, const std::function<void(const char *, const char *)> &onCompleted);
